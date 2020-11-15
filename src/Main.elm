75005fb847d9506b30f37d70ba04508696ed8d2a
tsrapnik <yannick-peeters@outlook.com>
2020-11-15 13:51:25 +0100

    reorganized code.

--- src/Main.elm
+++ src/Main.elm
@@ -15,118 +15,50 @@ import Json.Encode as Encode
 import Task
 
 
-
---TODO: add dates on clear.
-
-
-type alias TimeInMinutes =
-    Int
+port setStorage : Encode.Value -> Cmd msg
 
 
-invalidTimeString : String
-invalidTimeString =
-    "--:--"
+main : Program Encode.Value Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
 
 
-{-| output is in the format hh:mm, where mm
-can be 00 to 59 and hh can be 00 to 99. if the int cannot
-get converted to this format --:-- is returned.
--}
-minutesToString : TimeInMinutes -> String
-minutesToString timeInMinutes =
-    let
-        minMinutes =
-            0
-
-        maxMinutes =
-            99 * 60 + 59
-    in
-    if (timeInMinutes > maxMinutes) || (timeInMinutes < minMinutes) then
-        invalidTimeString
-
-    else
-        let
-            hours =
-                timeInMinutes // 60
-
-            remainingMinutes =
-                remainderBy 60 timeInMinutes
-
-            --we have already checked if int has valid values.
-            intToTwoDigitString : Int -> String
-            intToTwoDigitString int =
-                if int < 10 then
-                    "0" ++ String.fromInt int
-
-                else
-                    String.fromInt int
-        in
-        intToTwoDigitString hours ++ ":" ++ intToTwoDigitString remainingMinutes
+type Msg
+    = AddTask DayIndex
+    | RemoveTask DayIndex TaskIndex
+    | SetProject DayIndex TaskIndex String
+    | SetComment DayIndex TaskIndex String
+    | SetStartTime DayIndex TaskIndex String
+    | SetStopTime DayIndex TaskIndex String
+    | Save
+    | Load
+    | Loaded File
+    | Parsed String
+    | Clear
+    | ReceiveDate Date
 
 
-{-| convert format hh:mm to int in minutes. hh can be from 00 to 99 and minute from
-00 to 59. all other formats return a nothing value.
--}
-stringToMinutes : String -> Maybe TimeInMinutes
-stringToMinutes string =
-    let
-        maybeHours =
-            String.toInt (String.slice 0 2 string)
 
-        separatorIsCorrect =
-            String.slice 2 3 string == ":"
-
-        maybeMinutes =
-            String.toInt (String.slice 3 5 string)
-    in
-    case ( maybeHours, separatorIsCorrect, maybeMinutes ) of
-        ( Just hours, True, Just minutes ) ->
-            if (hours < 0) || (hours > 99) || (minutes < 0) || (minutes > 59) then
-                Nothing
-
-            else
-                Just (hours * 60 + minutes)
-
-        _ ->
-            Nothing
+{- model -}
 
 
-taskTime : Task -> Maybe TimeInMinutes
-taskTime task =
-    case ( task.startTime, task.stopTime ) of
-        ( Just startTime, Just stopTime ) ->
-            if (stopTime - startTime) < 0 then
-                Nothing
-
-            else
-                Just (stopTime - startTime)
-
-        _ ->
-            Nothing
+type alias Model =
+    { days : Array Day
+    , startDate : Maybe Date
+    }
 
 
-dailyWorktime : Day -> Maybe TimeInMinutes
-dailyWorktime day =
-    let
-        maybeAdd : Maybe TimeInMinutes -> Maybe TimeInMinutes -> Maybe TimeInMinutes
-        maybeAdd first second =
-            case ( first, second ) of
-                ( Just firstTime, Just secondTime ) ->
-                    Just (firstTime + secondTime)
-
-                _ ->
-                    Nothing
-    in
-    day.tasks
-        |> Array.map taskTime
-        |> Array.foldl maybeAdd (Just 0)
+type alias Day =
+    { tasks : Array Task
+    }
 
 
 type alias DayIndex =
-    Int
-
-
-type alias TaskIndex =
     Int
 
 
@@ -138,6 +70,35 @@ type alias Task =
     }
 
 
+type alias TaskIndex =
+    Int
+
+
+type alias TimeInMinutes =
+    Int
+
+
+init : Encode.Value -> ( Model, Cmd Msg )
+init flags =
+    case Decode.decodeValue decoder flags of
+        Ok model ->
+            ( model
+            , Cmd.none
+            )
+
+        Err _ ->
+            ( { days = emptyWeek
+              , startDate = Nothing
+              }
+            , Task.perform ReceiveDate Date.today
+            )
+
+
+emptyWeek : Array Day
+emptyWeek =
+    Array.repeat 5 { tasks = Array.empty }
+
+
 emptyTask : Maybe TimeInMinutes -> Task
 emptyTask startTime =
     { project = ""
@@ -145,6 +106,85 @@ emptyTask startTime =
     , startTime = startTime
     , stopTime = Nothing
     }
+
+
+
+{- view -}
+
+
+view : Model -> Html Msg
+view model =
+    let
+        requiredDailyWorkTime =
+            8 * 60
+
+        requiredWorkTime passedWorkTime =
+            if passedWorkTime == 0 then
+                0
+
+            else
+                requiredDailyWorkTime - passedWorkTime
+
+        addRequiredMinutesToArray : Day -> Array (Maybe TimeInMinutes) -> Array (Maybe TimeInMinutes)
+        addRequiredMinutesToArray day array =
+            case ( dailyWorktime day, Array.get (Array.length array - 1) array ) of
+                ( Just workTime, Nothing ) ->
+                    Array.push (Just (requiredWorkTime workTime)) array
+
+                ( Just workTime, Just (Just accumulatedTime) ) ->
+                    Array.push (Just (requiredWorkTime workTime + accumulatedTime)) array
+
+                _ ->
+                    Array.push Nothing array
+
+        requiredMinutes : Array (Maybe TimeInMinutes)
+        requiredMinutes =
+            Array.foldl addRequiredMinutesToArray Array.empty model.days
+
+        daysData : Array ( Maybe TimeInMinutes, Day )
+        daysData =
+            Array.Extra.zip requiredMinutes model.days
+
+        dayDataToHtml : Maybe Date -> Int -> ( Maybe TimeInMinutes, Day ) -> Html Msg
+        dayDataToHtml startDate dayIndex dayData =
+            viewDay startDate (Tuple.first dayData) dayIndex (Tuple.second dayData)
+    in
+    div []
+        [ div [ class "week" ]
+            (Array.toList
+                (Array.indexedMap (dayDataToHtml model.startDate) daysData)
+            )
+        , div [ class "loadSave" ]
+            [ button [ class "load", onClick Save ] [ text "save" ]
+            , button [ class "save", onClick Load ] [ text "load" ]
+            , button [ class "clear", onClick Clear ] [ text "clear" ]
+            ]
+        ]
+
+
+viewDay : Maybe Date -> Maybe TimeInMinutes -> DayIndex -> Day -> Html Msg
+viewDay startDate requiredMinutes dayIndex day =
+    div [ class "day" ]
+        [ div [] [ text (dayIndexToString dayIndex) ]
+        , case startDate of
+            Just date ->
+                div [] [ text <| Date.toIsoString <| Date.add Date.Days dayIndex date ]
+
+            Nothing ->
+                div [] []
+        , div [ class "tasks" ] (Array.toList (Array.indexedMap (\taskIndex task -> viewTask dayIndex taskIndex task) day.tasks))
+        , case requiredMinutes of
+            Just minutes ->
+                if minutes > 0 then
+                    time [ class "required_minutes_red" ] [ text (minutesToString minutes) ]
+
+                else
+                    time [ class "required_minutes_green" ] [ text (minutesToString -minutes) ]
+
+            Nothing ->
+                time [ class "required_minutes_white" ] [ text invalidTimeString ]
+        , button [ onClick (AddTask dayIndex) ] [ text "add task" ]
+        ]
 
 
 viewTask : DayIndex -> TaskIndex -> Task -> Html Msg
@@ -200,158 +240,8 @@ viewTask dayIndex taskIndex task =
         ]
 
 
-type alias Day =
-    { tasks : Array Task
-    }
 
-
-dayIndexToString : DayIndex -> String
-dayIndexToString dayIndex =
-    case dayIndex of
-        0 ->
-            "monday"
-
-        1 ->
-            "tuesday"
-
-        2 ->
-            "wednesday"
-
-        3 ->
-            "thursday"
-
-        4 ->
-            "friday"
-
-        _ ->
-            "unknown day"
-
-
-viewDay : Maybe Date -> Maybe TimeInMinutes -> DayIndex -> Day -> Html Msg
-viewDay startDate requiredMinutes dayIndex day =
-    div [ class "day" ]
-        [ div [] [ text (dayIndexToString dayIndex) ]
-        , case startDate of
-            Just date ->
-                div [] [ text <| Date.toIsoString <| Date.add Date.Days dayIndex date ]
-
-            Nothing ->
-                div [] []
-        , div [ class "tasks" ] (Array.toList (Array.indexedMap (\taskIndex task -> viewTask dayIndex taskIndex task) day.tasks))
-        , case requiredMinutes of
-            Just minutes ->
-                if minutes > 0 then
-                    time [ class "required_minutes_red" ] [ text (minutesToString minutes) ]
-
-                else
-                    time [ class "required_minutes_green" ] [ text (minutesToString -minutes) ]
-
-            Nothing ->
-                time [ class "required_minutes_white" ] [ text invalidTimeString ]
-        , button [ onClick (AddTask dayIndex) ] [ text "add task" ]
-        ]
-
-
-main : Program Encode.Value Model Msg
-main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = \_ -> Sub.none
-        }
-
-
-type alias Model =
-    { days : Array Day
-    , startDate : Maybe Date
-    }
-
-
-emptyWeek : Array Day
-emptyWeek =
-    Array.repeat 5 { tasks = Array.empty }
-
-
-init : Encode.Value -> ( Model, Cmd Msg )
-init flags =
-    case Decode.decodeValue decoder flags of
-        Ok model ->
-            ( model
-            , Cmd.none
-            )
-
-        Err _ ->
-            ( { days = emptyWeek
-              , startDate = Nothing
-              }
-            , Task.perform ReceiveDate Date.today
-            )
-
-
-view : Model -> Html Msg
-view model =
-    let
-        requiredDailyWorkTime =
-            8 * 60
-
-        requiredWorkTime passedWorkTime =
-            if passedWorkTime == 0 then
-                0
-
-            else
-                requiredDailyWorkTime - passedWorkTime
-
-        addRequiredMinutesToArray : Day -> Array (Maybe TimeInMinutes) -> Array (Maybe TimeInMinutes)
-        addRequiredMinutesToArray day array =
-            case ( dailyWorktime day, Array.get (Array.length array - 1) array ) of
-                ( Just workTime, Nothing ) ->
-                    Array.push (Just (requiredWorkTime workTime)) array
-
-                ( Just workTime, Just (Just accumulatedTime) ) ->
-                    Array.push (Just (requiredWorkTime workTime + accumulatedTime)) array
-
-                _ ->
-                    Array.push Nothing array
-
-        requiredMinutes : Array (Maybe TimeInMinutes)
-        requiredMinutes =
-            Array.foldl addRequiredMinutesToArray Array.empty model.days
-
-        daysData : Array ( Maybe TimeInMinutes, Day )
-        daysData =
-            Array.Extra.zip requiredMinutes model.days
-
-        dayDataToHtml : Maybe Date -> Int -> ( Maybe TimeInMinutes, Day ) -> Html Msg
-        dayDataToHtml startDate dayIndex dayData =
-            viewDay startDate (Tuple.first dayData) dayIndex (Tuple.second dayData)
-    in
-    div []
-        [ div [ class "week" ]
-            (Array.toList
-                (Array.indexedMap (dayDataToHtml model.startDate) daysData)
-            )
-        , div [ class "loadSave" ]
-            [ button [ class "load", onClick Save ] [ text "save" ]
-            , button [ class "save", onClick Load ] [ text "load" ]
-            , button [ class "clear", onClick Clear ] [ text "clear" ]
-            ]
-        ]
-
-
-type Msg
-    = AddTask DayIndex
-    | RemoveTask DayIndex TaskIndex
-    | SetProject DayIndex TaskIndex String
-    | SetComment DayIndex TaskIndex String
-    | SetStartTime DayIndex TaskIndex String
-    | SetStopTime DayIndex TaskIndex String
-    | Save
-    | Load
-    | Loaded File
-    | Parsed String
-    | Clear
-    | ReceiveDate Date
+{- update -}
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -480,12 +370,13 @@ update msg model =
                     case model.startDate of
                         Just date ->
                             Date.format "YYYY_w" date
+
                         Nothing ->
                             ""
+
                 fileName =
                     "working_hours_" ++ yearAndWeek ++ ".json"
             in
-            
             ( model
             , Download.string fileName "application/json" (Encode.encode 4 (encode model))
             )
@@ -532,6 +423,217 @@ update msg model =
             ( newModel
             , setStorage (encode newModel)
             )
+
+
+
+{- json -}
+
+
+encode : Model -> Encode.Value
+encode model =
+    let
+        startDate =
+            case model.startDate of
+                Just date ->
+                    [ ( "startDate", Encode.int (Date.toRataDie date) ) ]
+
+                Nothing ->
+                    []
+    in
+    Encode.object
+        (List.concat
+            [ [ ( "days", Encode.array encodeDay model.days ) ]
+            , startDate
+            ]
+        )
+
+
+encodeDay : Day -> Encode.Value
+encodeDay day =
+    Encode.object
+        [ ( "tasks", Encode.array encodeTask day.tasks )
+        ]
+
+
+encodeTask : Task -> Encode.Value
+encodeTask task =
+    let
+        startTime =
+            case task.startTime of
+                Just minutes ->
+                    [ ( "startTime", Encode.int minutes ) ]
+
+                Nothing ->
+                    []
+
+        stopTime =
+            case task.stopTime of
+                Just minutes ->
+                    [ ( "stopTime", Encode.int minutes ) ]
+
+                Nothing ->
+                    []
+    in
+    Encode.object
+        (List.concat
+            [ [ ( "project", Encode.string task.project )
+              , ( "comment", Encode.string task.comment )
+              ]
+            , startTime
+            , stopTime
+            ]
+        )
+
+
+decoder : Decode.Decoder Model
+decoder =
+    Decode.map2 Model
+        (Decode.field "days" (Decode.array dayDecoder))
+        (Decode.maybe <| Decode.map Date.fromRataDie <| Decode.field "startDate" Decode.int)
+
+
+dayDecoder : Decode.Decoder Day
+dayDecoder =
+    Decode.map Day
+        (Decode.field "tasks" (Decode.array taskDecoder))
+
+
+taskDecoder : Decode.Decoder Task
+taskDecoder =
+    Decode.map4 Task
+        (Decode.field "project" Decode.string)
+        (Decode.field "comment" Decode.string)
+        (Decode.maybe (Decode.field "startTime" Decode.int))
+        (Decode.maybe (Decode.field "stopTime" Decode.int))
+
+
+
+{- conversions -}
+
+
+invalidTimeString : String
+invalidTimeString =
+    "--:--"
+
+
+{-| output is in the format hh:mm, where mm
+can be 00 to 59 and hh can be 00 to 99. if the int cannot
+get converted to this format --:-- is returned.
+-}
+minutesToString : TimeInMinutes -> String
+minutesToString timeInMinutes =
+    let
+        minMinutes =
+            0
+
+        maxMinutes =
+            99 * 60 + 59
+    in
+    if (timeInMinutes > maxMinutes) || (timeInMinutes < minMinutes) then
+        invalidTimeString
+
+    else
+        let
+            hours =
+                timeInMinutes // 60
+
+            remainingMinutes =
+                remainderBy 60 timeInMinutes
+
+            --we have already checked if int has valid values.
+            intToTwoDigitString : Int -> String
+            intToTwoDigitString int =
+                if int < 10 then
+                    "0" ++ String.fromInt int
+
+                else
+                    String.fromInt int
+        in
+        intToTwoDigitString hours ++ ":" ++ intToTwoDigitString remainingMinutes
+
+
+{-| convert format hh:mm to int in minutes. hh can be from 00 to 99 and minute from
+00 to 59. all other formats return a nothing value.
+-}
+stringToMinutes : String -> Maybe TimeInMinutes
+stringToMinutes string =
+    let
+        maybeHours =
+            String.toInt (String.slice 0 2 string)
+
+        separatorIsCorrect =
+            String.slice 2 3 string == ":"
+
+        maybeMinutes =
+            String.toInt (String.slice 3 5 string)
+    in
+    case ( maybeHours, separatorIsCorrect, maybeMinutes ) of
+        ( Just hours, True, Just minutes ) ->
+            if (hours < 0) || (hours > 99) || (minutes < 0) || (minutes > 59) then
+                Nothing
+
+            else
+                Just (hours * 60 + minutes)
+
+        _ ->
+            Nothing
+
+
+dayIndexToString : DayIndex -> String
+dayIndexToString dayIndex =
+    case dayIndex of
+        0 ->
+            "monday"
+
+        1 ->
+            "tuesday"
+
+        2 ->
+            "wednesday"
+
+        3 ->
+            "thursday"
+
+        4 ->
+            "friday"
+
+        _ ->
+            "unknown day"
+
+
+
+{- calculations -}
+
+
+taskTime : Task -> Maybe TimeInMinutes
+taskTime task =
+    case ( task.startTime, task.stopTime ) of
+        ( Just startTime, Just stopTime ) ->
+            if (stopTime - startTime) < 0 then
+                Nothing
+
+            else
+                Just (stopTime - startTime)
+
+        _ ->
+            Nothing
+
+
+dailyWorktime : Day -> Maybe TimeInMinutes
+dailyWorktime day =
+    let
+        maybeAdd : Maybe TimeInMinutes -> Maybe TimeInMinutes -> Maybe TimeInMinutes
+        maybeAdd first second =
+            case ( first, second ) of
+                ( Just firstTime, Just secondTime ) ->
+                    Just (firstTime + secondTime)
+
+                _ ->
+                    Nothing
+    in
+    day.tasks
+        |> Array.map taskTime
+        |> Array.foldl maybeAdd (Just 0)
 
 
 {-| take an array and replace element at given index with zero or more elements defined by a
@@ -615,84 +717,3 @@ adaptToLunch task =
 
         _ ->
             Array.fromList [ task ]
-
-
-port setStorage : Encode.Value -> Cmd msg
-
-
-encode : Model -> Encode.Value
-encode model =
-    let
-        startDate =
-            case model.startDate of
-                Just date ->
-                    [ ( "startDate", Encode.int (Date.toRataDie date) ) ]
-
-                Nothing ->
-                    []
-    in
-    Encode.object
-        (List.concat
-            [ [ ( "days", Encode.array encodeDay model.days ) ]
-            , startDate
-            ]
-        )
-
-
-encodeDay : Day -> Encode.Value
-encodeDay day =
-    Encode.object
-        [ ( "tasks", Encode.array encodeTask day.tasks )
-        ]
-
-
-encodeTask : Task -> Encode.Value
-encodeTask task =
-    let
-        startTime =
-            case task.startTime of
-                Just minutes ->
-                    [ ( "startTime", Encode.int minutes ) ]
-
-                Nothing ->
-                    []
-
-        stopTime =
-            case task.stopTime of
-                Just minutes ->
-                    [ ( "stopTime", Encode.int minutes ) ]
-
-                Nothing ->
-                    []
-    in
-    Encode.object
-        (List.concat
-            [ [ ( "project", Encode.string task.project )
-              , ( "comment", Encode.string task.comment )
-              ]
-            , startTime
-            , stopTime
-            ]
-        )
-
-
-decoder : Decode.Decoder Model
-decoder =
-    Decode.map2 Model
-        (Decode.field "days" (Decode.array dayDecoder))
-        (Decode.maybe <| Decode.map Date.fromRataDie <| Decode.field "startDate" Decode.int)
-
-
-dayDecoder : Decode.Decoder Day
-dayDecoder =
-    Decode.map Day
-        (Decode.field "tasks" (Decode.array taskDecoder))
-
-
-taskDecoder : Decode.Decoder Task
-taskDecoder =
-    Decode.map4 Task
-        (Decode.field "project" Decode.string)
-        (Decode.field "comment" Decode.string)
-        (Decode.maybe (Decode.field "startTime" Decode.int))
-        (Decode.maybe (Decode.field "stopTime" Decode.int))
